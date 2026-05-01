2dd16268112600155977f73fd59d3af299de460c
Khulood33 <202111142@gcet.edu.om>
2026-05-01 21:46:40 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhiJlxxruZdx0z5jmtw1E6aSRTFT7hu8s",
+  apiKey: "AIzaSyDhiJlxrruZdx0z5jmtw1E6aSRTFT7hu8s",
   authDomain: "oman-tourism-new.firebaseapp.com",
   projectId: "oman-tourism-new",
   storageBucket: "oman-tourism-new.firebasestorage.app",
   messagingSenderId: "580317693824",
-  appId: "1:580317693824:web:b8b88fff7048f955ca6365",
-  measurementId: "G-N83D9GRMVC"
+  appId: "1:580317693824:web:b8b88fff7048f955ca6365"
 };
 
 const app = initializeApp(firebaseConfig);
